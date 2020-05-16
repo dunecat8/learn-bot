1aebfb89b67816eac2dfc862a80eab0fe69fbd86
Valeria <valery.y.s@gmail.com>
2020-05-17 00:41:52 +0300

    new bot

---
+++ bot.py
@@ -0,0 +1,29 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+PROXY = {'proxy_url': settings.PROXY_URL,
+         'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password':settings.PROXY_PASSWORD}}
+
+def greet_user(update, context):
+    print('Там кто-то вызвал бота..')
+    update.message.reply_text('Вы пришли и запустили бота. Бот счастлив')
+
+def talk_to_me(update, context):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    logging.info('bot launched')
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ == '__main__':
+    main()
